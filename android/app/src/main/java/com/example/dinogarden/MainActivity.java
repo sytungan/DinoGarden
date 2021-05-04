684ee797de5be7f24aa7575f3676896e3c54b88c
Ankun <sytungan@gmail.com>
2021-05-04 23:53:54 +0700

    Add dependencies and projects

--- android/app/src/main/java/com/example/dinogarden/MainActivity.java
+++ android/app/src/main/java/com/example/dinogarden/MainActivity.java
@@ -2,5 +2,6 @@ package com.example.dinogarden;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity implements SerialInputOutputManager.Listener {
+
 }
