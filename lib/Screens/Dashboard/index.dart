8875b6acc47ad2f9b20c288cf771a29411985e17
SoiAhihi <62455700+SoiAhihi@users.noreply.github.com>
2021-06-14 14:53:19 +0700

    calendar

--- lib/Screens/Dashboard/index.dart
+++ lib/Screens/Dashboard/index.dart
@@ -14,11 +14,11 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-Future<Log> fetchAlbum() async {
+Future<Log> fetchAlbum(String userID) async {
   final response =
       await http.post(Uri.parse('https://testdinogarden.herokuapp.com/log/get'),
           //user id here
-          body: {'userId': '60c42eaf98ca9c0015be8cb5'}
+          body: {'userId': userID}
           // body: {'userId': '60c4c686a7f9bf001569fb93'}
           );
 
@@ -33,50 +33,53 @@ Future<Log> fetchAlbum() async {
   }
 }
 
-
-
 ////////////////////////////////////////////
-class dashboardScrean extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
+  String userID;
+  String gardenName;
+  DashboardScreen(this.userID, this.gardenName);
+  @override
+  dashboardScrean createState() => dashboardScrean(userID);
+}
+
+class dashboardScrean extends State<DashboardScreen> {
   int _currentIndex = 1;
+  String userID;
+  dashboardScrean(this.userID);
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'dashboardScrean',
-          color: Color.fromRGBO(255, 255, 255, 1),
-          home: Scaffold(
-            appBar: tabBar(),
-            body:Center(
-              child: FutureBuilder<Log>(
-                future: fetchAlbum(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    List<dynamic> log = snapshot.data.data;
-                    // Map a= json.decode(post[2]['19 4 2021']);
-                    return ListView(children: <Widget>[
-                      Container(height: 130.0, child: Calendar()),
-                      Container(height: 40.0, child: BtViewCalendar(log)),
-                      Container(height: 70.0, child: Insight()),
-                      Container(height: 400.0, child: GraphGroup()),
-                      /////////////////
-                      // Container(height: 100,child: Text(snapshot.data.data.toString()),)
-                      ////////////////
-                    ])
-                    ;
-                  } else if (snapshot.hasError) {
-                    return Text("${snapshot.error}");
-                  }
-                  // By default, show a loading spinner.
-                  return CircularProgressIndicator();
-                },
-              ),
-            ) ,
-
-
-
-            bottomNavigationBar: bottomNavigator(context, _currentIndex),
-          ));
-
-
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'dashboardScrean',
+        color: Color.fromRGBO(255, 255, 255, 1),
+        home: Scaffold(
+          appBar: tabBar(),
+          body: Center(
+            child: FutureBuilder<Log>(
+              future: fetchAlbum(userID),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<dynamic> log = snapshot.data.data;
+                  // Map a= json.decode(post[2]['19 4 2021']);
+                  return ListView(children: <Widget>[
+                    Container(height: 130.0, child: Calendar()),
+                    Container(height: 40.0, child: BtViewCalendar(log)),
+                    Container(height: 70.0, child: Insight()),
+                    Container(height: 400.0, child: GraphGroup()),
+                    /////////////////
+                    // Container(height: 100,child: Text(snapshot.data.data.toString()),)
+                    ////////////////
+                  ]);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                // By default, show a loading spinner.
+                return CircularProgressIndicator();
+              },
+            ),
+          ),
+          bottomNavigationBar: bottomNavigator(context, _currentIndex,
+              userID: widget.userID, userName: widget.gardenName),
+        ));
   }
 }
