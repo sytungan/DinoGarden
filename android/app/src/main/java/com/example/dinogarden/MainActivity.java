c4524606f5382fa05346724ea39f028fd80aeaba
Ankun <sytungan@gmail.com>
2021-07-23 20:07:45 +0700

    Module android port uart and androidMQTT deprecated

--- android/app/src/main/java/com/example/dinogarden/MainActivity.java
+++ android/app/src/main/java/com/example/dinogarden/MainActivity.java
@@ -42,139 +42,142 @@ import java.util.concurrent.Executors;
 
 //https://github.com/rcties/PrinterPlusCOMM
 //https://github.com/mik3y/usb-serial-for-android
-public class MainActivity extends FlutterActivity  implements SerialInputOutputManager.Listener{
-    private static final String ACTION_USB_PERMISSION = "com.android.recipes.USB_PERMISSION";
-    private static final String INTENT_ACTION_GRANT_USB = "com.example.dinogarden" + ".GRANT_USB";
-    UsbSerialPort port;
+public class MainActivity extends FlutterActivity {
 
-    MQTTService mqttService;
-
-    private Thread readThread;
-
-    private boolean mRunning;
-
-
-    //private UsbHidDevice device = null;
-
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // MQTT Service
-
-        mqttService = new MQTTService(this);
-        mqttService.setCallback(new MqttCallbackExtended() {
-            @Override
-            public void connectComplete(boolean reconnect, String serverURI) {
-
-            }
-
-            @Override
-            public void connectionLost( Throwable throwable){
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                String data_to_microbit = message.toString();
-                Log.w("Mqtt", data_to_microbit);
-                // Json parser
-            }
-
-            @Override
-            public void deliveryComplete( IMqttDeliveryToken token) {
-
-            }
-        });
-
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-
-
-        if (availableDrivers.isEmpty()) {
-            Log.d("UART", "UART is not available");
-            Toast.makeText(MainActivity.this, "UART is not available", Toast.LENGTH_LONG).show();
-        }else {
-            Log.d("UART", "UART is available");
-            Toast.makeText(MainActivity.this, "UART is available", Toast.LENGTH_LONG).show();
-            UsbSerialDriver driver = availableDrivers.get(0);
-            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-            if (connection == null) {
-                PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(INTENT_ACTION_GRANT_USB), 0);
-                manager.requestPermission(driver.getDevice(), usbPermissionIntent);
-                manager.requestPermission(driver.getDevice(), PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0));
-            } else {
-                // Most devices have just one port (port 0)
-                port = driver.getPorts().get(0);
-                try {
-                    port.open(connection);
-                    port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-                    port.write("hello abc#".getBytes(), 1000);
-                    Toast.makeText(MainActivity.this, "A sample string is sent", Toast.LENGTH_LONG).show();
-
-                    SerialInputOutputManager usbIoManager = new SerialInputOutputManager(port, this);
-                    Executors.newSingleThreadExecutor().submit(usbIoManager);
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "Sending a message is fail", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        try {
-            port.close();
-        }catch (Exception e) {
-
-        }
-    }
-
-
-    @Override
-    public void onNewData(final byte[] data) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("dataAA", Arrays.toString(data));
-            }
-        });
-    }
-
-    @Override
-    public void onRunError(Exception e) {
-
-    }
-
-    private void sendDataMQTT( String data){
-        MqttMessage msg = new MqttMessage();
-        msg.setId(1234);
-        msg.setQos(0);
-        msg.setRetained(true);
-
-        byte[] b = data.getBytes(Charset.forName("UTF-8"));
-        msg.setPayload(b);
-
-        Log.d("ABC","Publish: " + msg);
-        try {
-            mqttService.mqttAndroidClient.publish("[Your path to the feed you want to send message]", msg);
-        } catch ( MqttException e){
-            Log.d("MQTT", "sendDataMQTT: cannot send message");
-        }
-    }
 }
+// public class MainActivity extends FlutterActivity  implements SerialInputOutputManager.Listener{
+    // private static final String ACTION_USB_PERMISSION = "com.android.recipes.USB_PERMISSION";
+    // private static final String INTENT_ACTION_GRANT_USB = "com.example.dinogarden" + ".GRANT_USB";
+    // UsbSerialPort port;
+
+    // MQTTService mqttService;
+
+    // private Thread readThread;
+
+    // private boolean mRunning;
+
+
+    // //private UsbHidDevice device = null;
+
+    // private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    // public static String bytesToHex(byte[] bytes) {
+    //     char[] hexChars = new char[bytes.length * 2];
+    //     for (int j = 0; j < bytes.length; j++) {
+    //         int v = bytes[j] & 0xFF;
+    //         hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+    //         hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+    //     }
+    //     return new String(hexChars);
+    // }
+
+
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //     super.onCreate(savedInstanceState);
+    //     // MQTT Service
+
+    //     mqttService = new MQTTService(this);
+    //     mqttService.setCallback(new MqttCallbackExtended() {
+    //         @Override
+    //         public void connectComplete(boolean reconnect, String serverURI) {
+
+    //         }
+
+    //         @Override
+    //         public void connectionLost( Throwable throwable){
+
+    //         }
+
+    //         @Override
+    //         public void messageArrived(String topic, MqttMessage message) throws Exception {
+    //             String data_to_microbit = message.toString();
+    //             Log.w("Mqtt", data_to_microbit);
+    //             // Json parser
+    //         }
+
+    //         @Override
+    //         public void deliveryComplete( IMqttDeliveryToken token) {
+
+    //         }
+    //     });
+
+    //     UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+    //     List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+
+
+    //     if (availableDrivers.isEmpty()) {
+    //         Log.d("UART", "UART is not available");
+    //         Toast.makeText(MainActivity.this, "UART is not available", Toast.LENGTH_LONG).show();
+    //     }else {
+    //         Log.d("UART", "UART is available");
+    //         Toast.makeText(MainActivity.this, "UART is available", Toast.LENGTH_LONG).show();
+    //         UsbSerialDriver driver = availableDrivers.get(0);
+    //         UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+    //         if (connection == null) {
+    //             PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(INTENT_ACTION_GRANT_USB), 0);
+    //             manager.requestPermission(driver.getDevice(), usbPermissionIntent);
+    //             manager.requestPermission(driver.getDevice(), PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0));
+    //         } else {
+    //             // Most devices have just one port (port 0)
+    //             port = driver.getPorts().get(0);
+    //             try {
+    //                 port.open(connection);
+    //                 port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+    //                 port.write("hello abc#".getBytes(), 1000);
+    //                 Toast.makeText(MainActivity.this, "A sample string is sent", Toast.LENGTH_LONG).show();
+
+    //                 SerialInputOutputManager usbIoManager = new SerialInputOutputManager(port, this);
+    //                 Executors.newSingleThreadExecutor().submit(usbIoManager);
+    //             } catch (Exception e) {
+    //                 Toast.makeText(MainActivity.this, "Sending a message is fail", Toast.LENGTH_LONG).show();
+    //             }
+
+    //         }
+    //     }
+    // }
+
+    // @Override
+    // protected void onDestroy() {
+    //     super.onDestroy();
+    //     try {
+    //         port.close();
+    //     }catch (Exception e) {
+
+    //     }
+    // }
+
+
+    // @Override
+    // public void onNewData(final byte[] data) {
+    //     runOnUiThread(new Runnable() {
+    //         @Override
+    //         public void run() {
+    //             Log.d("dataAA", Arrays.toString(data));
+    //         }
+    //     });
+    // }
+
+    // @Override
+    // public void onRunError(Exception e) {
+
+    // }
+
+    // private void sendDataMQTT( String data){
+    //     MqttMessage msg = new MqttMessage();
+    //     msg.setId(1234);
+    //     msg.setQos(0);
+    //     msg.setRetained(true);
+
+    //     byte[] b = data.getBytes(Charset.forName("UTF-8"));
+    //     msg.setPayload(b);
+
+    //     Log.d("ABC","Publish: " + msg);
+    //     try {
+    //         mqttService.mqttAndroidClient.publish("[Your path to the feed you want to send message]", msg);
+    //     } catch ( MqttException e){
+    //         Log.d("MQTT", "sendDataMQTT: cannot send message");
+    //     }
+    // }
+// }
 
 
